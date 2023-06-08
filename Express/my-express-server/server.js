6e29f7b46241715af3363c8c5a3d415e2c022ad9
Anusha Byalyam Ramachandra <anushabyalyam@gmail.com>
2023-06-08 18:25:25 +0530

    Update server.js

--- Express/my-express-server/server.js
+++ Express/my-express-server/server.js
@@ -6,15 +6,15 @@ app.get("/", function(req, res) {
 });
 
 app.get("/contact", function(req, res) {
-    res.send("Contact me at anusha@gmail.com");
+    res.send("Contact me at anu@gmail.com");
 });
 
 app.get("/about", function(req, res) {
-    res.send("<h1>My name is Anusha, and I'm trying to achieve great things in life!</h1>");
+    res.send("<h1I am Anusha, and I am an aspiring full-stack developer.</h1>");
 });
 
 app.get("/hobbies", function(req, res) {
-    res.send("Code, Cry, Repeat");
+    res.send("Code, Pray, Play");
 });
 
 app.listen(3000, function() {
